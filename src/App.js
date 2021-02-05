3eff1c228a4bf391c1bc8f3aab8047194c033eb9
Luu-Kim-Hong <kimhongluu1501@gmail.com>
2021-02-05 23:38:30 +0700

    update_components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
+import HeaderClass from './Components/HeaderClass';
+import HeaderFunc from './Components/HeaderFunc';
 import PropsDemo from './Prop/PropsDemo';
 import BaitapRenderMap from './react_buoi3/BaitapRenderMap';
+
 
 function App() {
   return (
     <div className="App">
+      {/* <HeaderClass /> */}
+      {/* <HeaderFunc /> */}
       {/* <BaitapRenderMap /> */}
       <PropsDemo />
     </div>
